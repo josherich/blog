271bc6b64bfbea96199c395e1b475d4e66aa0900
josherich <josherichchen@gmail.com>
2016-11-18 01:44:49 +0800

    testing weibo redirect uri

--- mindynode/main.js
+++ mindynode/main.js
@@ -1,7 +1,10 @@
 document.querySelector('#signin #weibo_login').addEventListener('click', function() {
   console.log('weibo login');
-  let uri = 'http://www.chenhf.com/mindynode/auth.html';
-  window.location = `https://api.weibo.com/oauth2/authorize?client_id=2977966833&redirect_uri=${uri}`;
+  // let uri = 'http://www.chenhf.com/mindynode/auth.html';
+  // window.location = `https://api.weibo.com/oauth2/authorize?client_id=2977966833&redirect_uri=${uri}`;
+  WB2.login(function(data) {
+    console.log(data)
+  })
 });
 
 document.querySelector('#signin #qq_login').addEventListener('click', function() {
